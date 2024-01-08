816cd6706b2ca0b2994ade207b0b3518df130bfc
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-08 12:50:27 +0300

    ignore

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    username = models.TextField(max=25)
-    email = models.EmailField(max=50)
-    phone = models.CharField(max=14)
+    username = models.TextField(max_length=25)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=14)
 
     # get the full_name of user 
     def get_full_name(self):
