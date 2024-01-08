9ca4362eec3a2707d7a4482aa0ca65b9f3e59364
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-08 12:22:55 +0300

    user model

--- accounts/models.py
+++ accounts/models.py
@@ -6,4 +6,7 @@ class CustomUser(AbstractUser):
     username = models.TextField(max=25)
     email = models.EmailField(max=50)
     phone = models.CharField(max=14)
-    
+
+    # get the full_name of user 
+    def get_full_name(self):
+        return "{} {}".format(self.first_name, self.last_name)
