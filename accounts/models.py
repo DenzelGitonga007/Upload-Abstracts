fac78d220cad4191934f391134b067f92517c255
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-08 12:11:20 +0300

    auth_user_model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    username = models.TextField(max=25)
+    email = models.EmailField(max=50)
+    phone = models.CharField(max=14)
+    
