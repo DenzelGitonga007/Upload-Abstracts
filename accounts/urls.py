3bafcea6dfd19a9fe22101ae22f8557d941b01ee
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-09 11:37:56 +0300

    login

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ app_name = "accounts"
 
 urlpatterns = [
     path('register/', views.register, name="register"), # Register url
+    path('login/', views.user_login, name="login"), # Login url
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,43 @@
 from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm # For user creation
+from django.contrib.auth.forms import AuthenticationForm # Login form
+from django.contrib.auth import authenticate, login # For login
 from django.contrib import messages # For alert messages
 from .forms import CustomUserCreationForm
 # Create your views here.
 def register(request):
+    """Register user"""
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, "Congratulations! Your account is successfully registered. You can now log in...")
-            return redirect('login') # Redirect to login page
+            return redirect('accounts:login') # Redirect to login page
     else:
         form = CustomUserCreationForm()
 
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
+
+def user_login(request):
+    """Login user"""
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Welcome {}! You're now logged in.", format(username))
+                return redirect('home')
+            else:
+                messages.error(request, "Oops! Login error...Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+    else:
+        form = AuthenticationForm()
+    context = {'form': form}
+    return render(request, "accounts/login.html", context)
+
+
+
