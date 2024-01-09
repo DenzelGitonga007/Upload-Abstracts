d7690cab9486b7309a4b55ab6a21eb0afdd5adaa
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-09 11:19:07 +0300

    admin for accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+class CustomuserAdmin(admin.ModelAdmin):
+    """Customize admin interface for accounts, CustomUser model"""
+    list_display = ["username", "email", "phone", "is_staff"]
+    search_fields = ["username", "email"]
+
+admin.site.register(CustomUser, CustomuserAdmin)
