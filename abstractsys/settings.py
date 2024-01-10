46339aa13e7e0d05f73b0cfd28f252e1607da811
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-10 11:31:53 +0300

    install bootstrap

--- abstractsys/settings.py
+++ abstractsys/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # My apps
-    'accounts',
-    'abstracts',
+    'accounts', # User auth
+    'abstracts', # CRUD
+    'bootstrap5', # UI/UX
 ]
 
 MIDDLEWARE = [
