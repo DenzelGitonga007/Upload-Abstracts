b733f79e2b8bc954bc5ce3b106bd69a5d16aff97
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-12 16:52:45 +0300

    security key

--- abstractsys/settings.py
+++ abstractsys/settings.py
@@ -36,8 +36,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-v-t5^e66p=4(d(=9)oe)bq(+(#4$^ohj+@!gz8qc1ba5@!ojy@'
-SECRET_KEY = os.environ.get("SECRET_KEY") # set on deployment
+SECRET_KEY = 'django-insecure-v-t5^e66p=4(d(=9)oe)bq(+(#4$^ohj+@!gz8qc1ba5@!ojy@'
+# SECRET_KEY = os.environ.get("SECRET_KEY") # set on deployment
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
