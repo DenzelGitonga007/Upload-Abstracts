dd5bd5a0f40f669b4912b08c5defa92955f799e5
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-02-08 11:25:31 +0300

    deployment ready

--- build.sh
+++ build.sh
@@ -13,3 +13,22 @@
 # then
 #   python manage.py createsuperuser --no-input
 # fi
+
+
+
+
+# Go to the dashboard of the project and create the following environment variables:
+
+# Key = Value
+# -----------------
+# CREATE_SUPERUSER = True
+# DJANGO_SUPERUSER_EMAIL = <...>
+# DJANGO_SUPERUSER_PASSWORD = <...>
+# DJANGO_SUPERUSER_USERNAME =<...>
+# After that add the following to this build.sh script:
+
+# if [[ $CREATE_SUPERUSER ]];
+# then
+#   python world_champ_2022/manage.py createsuperuser --no-input
+# fi
+# Commit and push your changes
