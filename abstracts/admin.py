6a6e3fbde398b851ab1e5aa061662b0ffa0e3947
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-02-08 11:44:01 +0300

    download from admin

--- abstracts/admin.py
+++ abstracts/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Abstract
 
-# Register your models here.
 class AbstractAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'file']
+    list_display = ['title', 'author', 'display_file_link']
     search_fields = ['title', 'author', 'file', 'description']
+
+    def display_file_link(self, obj):
+        if obj.file:
+            file_url = obj.file.url
+            return format_html(f'<a href="{file_url}" download>Download</a>')
+        return "No File"
+
+    display_file_link.short_description = "Download File"
 
 admin.site.register(Abstract, AbstractAdmin)
